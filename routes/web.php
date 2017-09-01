1ff077896643cf8783a79398f2ad4e79b4cdf7e2
Laravel Shift <shift@laravelshift.com>
2017-09-01 13:06:13 +0000

    Adopt PSR-2 coding style
    The Laravel framework adopts the PSR-2 coding style in version 5.1.
    Laravel apps *should* adopt this coding style as well. Read the
    [PSR-2 coding style guide][1] for more details and check out [PHPCS][2]
    to use as a code formatting tool.
    
    [1]: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md
    [2]: https://github.com/squizlabs/PHP_CodeSniffer

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::prefix('laravel-modules')->group(function () {
     Route::get('/', function () {
         return redirect('laravel-modules/v1/introduction');
     });
-
 });
 
 Route::prefix('laravel-menus')->group(function () {
@@ -15,7 +14,6 @@ Route::prefix('laravel-menus')->group(function () {
     Route::get('/', function () {
         return redirect('laravel-menus/v1/introduction');
     });
-
 });
 
 Route::prefix('laravel-videoable')->group(function () {
@@ -23,7 +21,6 @@ Route::prefix('laravel-videoable')->group(function () {
     Route::get('/', function () {
         return redirect('laravel-videoable/v1/introduction');
     });
-
 });
 
 
